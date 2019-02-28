3550e14f7cfa99d7101aa17871bc7d18166e8c3b
dogukanavci <37452679+dogukanavci@users.noreply.github.com>
2019-02-28 16:00:30 -0500

    Initial push

---
+++ Tbot/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+      consumer_key:         '4dmduInKH2b3qBb9YUVyGwcPM',
+      consumer_secret:      'ypfs1BVJKL4zZ6Wv2K8mdT6Gx1Qu9I4G3ltPvLZdURwcefI8CO',
+      access_token:         '1098720134460522497-7PPG7AmNrqFlcxKYm08P2tEM4P9exh',
+      access_token_secret:  'AotbajxAfhE3vupklbUFbDyXtxtTOpw9ZteIFMQV4Zcz2',
+      timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+      strictSSL:            true     // optional - requires SSL certificates to be valid
+}
